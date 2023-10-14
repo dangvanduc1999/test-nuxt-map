8df24d99befbfd79bfae708a90f0f8039e293b11
ducdang <vanduclipi@gmail.com>
2023-10-14 17:52:44 +0700

    [fix] fix ui dropdown menu at home

--- components/header-module/Navbar.vue
+++ components/header-module/Navbar.vue
@@ -1,80 +1,46 @@
 <template>
-  <nav class="nav">
-    <ul>
-      <li>
-        <a-dropdown   :overlayClassName="'nav-menu-dropdown'">
-          <a href="#">Maps</a>
+    <nav class="nav">
+        <ul>
+            <li>
+                <a-dropdown v-model:open="openDropdown" :overlayClassName="'nav-menu-dropdown'"
+                            :destroyPopupOnHide="true">
+                    <p>Maps</p>
+                    <template class="nav-menu-dropdown" #overlay>
+                        <div class="navbar-wrapper-dropdown">
+                            <MapDropDownMenu/>
+                        </div>
+                    </template>
+                </a-dropdown>
+            </li>
+            <li>
+                <p>Apparel</p>
+            </li>
+            <li>
+                <p>Retailers</p>
+            </li>
+            <li>
+                <p>About </p>
+                <div class="js-dropdown dropdown">
+                    <div class="shell">
+                        <ul>
+                            <li>
+                                <p>About Us</p>
+                            </li>
 
-          <template class="nav-menu-dropdown" #overlay>
-              <a-collapse v-model:activeKey="activeKey" accordion>
-                <template #expandIcon>
-                  <PlusOutlined class="icon-collaspe"/>
-                </template>
-                <a-collapse-panel key="1" header="Fishing Map Guides">
-                  <p>A dog is a type of domesticated animal.Known for its loyalty and faithfulness,it can be found as a
-                    welcome guest in many households across the world.</p>
-                </a-collapse-panel>
-                <a-collapse-panel key="2" header="All-Outdoors Atlas & Field Guides  ">
-                  <p>A dog is a type of domesticated animal.Known for its loyalty and faithfulness,it can be found as a
-                    welcome guest in many households across the world.</p>
-                </a-collapse-panel>
-                <a-collapse-panel key="3" header="eBooks">
-                  <p>A dog is a type of domesticated animal.Known for its loyalty and faithfulness,it can be found as a
-                    welcome guest in many households across the world.</p>
-                </a-collapse-panel>
-                <a-collapse-panel key="4" header="Wood Engraved Maps">
-                  <p>A dog is a type of domesticated animal.Known for its loyalty and faithfulness,it can be found as a
-                    welcome guest in many households across the world.</p>
-                </a-collapse-panel>
-                <a-collapse-panel key="5" header="Maps By State">
+                            <li>
+                                <p>FAQS</p>
+                            </li>
 
-                  <span>Iowa</span>
-                  <span>Illinois</span>
-                  <span>Indiana</span>
-                  <span>Kentucky</span>
-                  <span>Michigan</span>
-                  <span>Minnesota</span>
-                  <span>Missouri</span>
-                  <span>New York</span>
-                  <span>South Dakota</span>
-                  <span>Tennessee</span>
-                  <span>Texas</span>
-                  <span>Wisconsin</span>
-
-                </a-collapse-panel>
-              </a-collapse>
-          </template>
-        </a-dropdown>
-      </li>
-      <li>
-        <a href="#">Apparel</a>
-      </li>
-      <li>
-        <a href="#">Retailers</a>
-      </li>
-      <li>
-        <a href="#">About </a>
-        <div class="js-dropdown dropdown">
-          <div class="shell">
-            <ul>
-              <li>
-                <a href="#">About Us</a>
-              </li>
-
-              <li>
-                <a href="#">FAQS</a>
-              </li>
-
-              <li>
-                <a href="#">Wholesale</a>
-              </li>
-            </ul>
-          </div><!-- /.shell -->
-        </div><!-- /.dropdown -->
-      </li>
-      <li>
-        <a href="#">Contact Us</a>
-      </li>
+                            <li>
+                                <p>Wholesale</p>
+                            </li>
+                        </ul>
+                    </div><!-- /.shell -->
+                </div><!-- /.dropdown -->
+            </li>
+            <li>
+                <p>Contact Us</p>
+            </li>
     </ul>
   </nav>
 
@@ -82,9 +48,9 @@
 </template>
 
 <script setup lang="ts">
-import {PlusOutlined} from "@ant-design/icons-vue"
+import MapDropDownMenu from "~/components/header-module/MapDropDownMenu.vue";
 
-const activeKey = ref(['5']);
+const openDropdown = ref<boolean>(false)
 
 
 </script>
